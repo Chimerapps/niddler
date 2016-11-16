3b287856a44ff4ef803d8c9722a5a13cc103dd61
Nicola Verbeeck <nicola@chimerapps.com>
2016-11-16 14:52:50 +0100

    Unused children array

--- niddler-ui/src/main/kotlin/com/icapps/niddler/ui/model/ui/NiddlerMessageTreeNode.kt
+++ niddler-ui/src/main/kotlin/com/icapps/niddler/ui/model/ui/NiddlerMessageTreeNode.kt
@@ -2,15 +2,12 @@ package com.icapps.niddler.ui.model.ui
 
 import com.icapps.niddler.ui.model.ParsedNiddlerMessage
 import javax.swing.tree.DefaultMutableTreeNode
-import javax.swing.tree.TreeNode
 
 /**
  * @author Nicola Verbeeck
  * @date 15/11/16.
  */
 class NiddlerMessageTreeNode(val item: ParsedNiddlerMessage) : DefaultMutableTreeNode() {
-
-    private val children: MutableList<TreeNode> = arrayListOf()
 
     init {
         initResponseSubItems(item)
