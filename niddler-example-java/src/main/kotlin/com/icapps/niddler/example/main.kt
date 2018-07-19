aa498f42d3c5ce8563da9f382b8cb1b731bfaf05
Nicola Verbeeck <nicola@chimerapps.com>
2018-07-19 20:23:07 +0200

    Added post with form body to java example

--- niddler-example-java/src/main/kotlin/com/icapps/niddler/example/main.kt
+++ niddler-example-java/src/main/kotlin/com/icapps/niddler/example/main.kt
@@ -3,6 +3,7 @@ package com.icapps.niddler.example
 import com.icapps.niddler.core.JavaNiddler
 import com.icapps.niddler.core.Niddler
 import com.icapps.niddler.interceptor.okhttp.NiddlerOkHttpInterceptor
+import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.util.logging.LogManager
@@ -43,11 +44,17 @@ fun main(args: Array<String>) {
         .url("https://jsonplaceholder.typicode.com/posts")
         .build()
     val request2 = request.newBuilder().build()
+    val request3 = Request.Builder()
+        .url("http://httpbin.org/post")
+        .post(FormBody.Builder().add("token", "add10 92201").add("type", "custom").build())
+        .build()
 
     val response1 = okHttp.newCall(request).execute()
     println("Request 1 executed (" + response1.code() + ")")
     val response2 = okHttp.newCall(request2).execute()
     println("Request 2 executed (" + response2.code() + ")")
+    val response3 = okHttp.newCall(request3).execute()
+    println("Request 3 executed (" + response3.code() + ")")
 
     println("Press return to stop")
     System.`in`.read()
