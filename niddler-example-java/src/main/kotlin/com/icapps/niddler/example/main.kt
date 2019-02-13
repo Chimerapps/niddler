6b962abc6741cec07bf4bd02d24794b46edaa941
Nicola Verbeeck <nicola@chimerapps.com>
2019-02-13 20:29:10 +0100

    Update example with new features for the UI

--- niddler-example-java/src/main/kotlin/com/icapps/niddler/example/main.kt
+++ niddler-example-java/src/main/kotlin/com/icapps/niddler/example/main.kt
@@ -4,8 +4,11 @@ import com.icapps.niddler.core.JavaNiddler
 import com.icapps.niddler.core.Niddler
 import com.icapps.niddler.interceptor.okhttp.NiddlerOkHttpInterceptor
 import okhttp3.FormBody
+import okhttp3.HttpUrl
+import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
 import java.util.logging.LogManager
 
 
@@ -17,7 +20,7 @@ fun main(args: Array<String>) {
 
     val logManager = LogManager.getLogManager()
     JavaNiddler::class.java.getResourceAsStream("/logging.properties")
-            .use({ `is` -> logManager.readConfiguration(`is`) })
+            .use { `is` -> logManager.readConfiguration(`is`) }
 
     val niddler = JavaNiddler.Builder()
             .setPort(10299)
@@ -50,7 +53,11 @@ fun main(args: Array<String>) {
             .build()
     val request4 = Request.Builder()
             .get()
-            .url("http://httpbin.org/xml")
+            .url(HttpUrl.parse("http://httpbin.org/xml")!!.newBuilder().addQueryParameter("param","=102:~19em/%;").build())
+            .build()
+    val request5 = Request.Builder()
+            .url("http://httpbin.org/post")
+            .post(RequestBody.create(MediaType.parse("application/json"),"{\"nullValue\":null}"))
             .build()
 
     val response1 = okHttp.newCall(request).execute()
@@ -61,6 +68,8 @@ fun main(args: Array<String>) {
     println("Request 3 executed (" + response3.code() + ")")
     val response4 = okHttp.newCall(request4).execute()
     println("Request 4 executed (" + response4.code() + ")")
+    val response5 = okHttp.newCall(request5).execute()
+    println("Request 5 executed (" + response5.code() + ")")
 
     println("Press return to stop")
     System.`in`.read()
