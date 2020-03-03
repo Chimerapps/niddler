da1c6e353d5d9237220697a3630420de620e8100
Nicola Verbeeck <nicola@chimerapps.com>
2020-03-03 20:24:03 +0100

    Updated publish script

--- publishLibs.sh
+++ publishLibs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./gradlew :niddler-lib:clean :niddler-lib:assembleRelease :niddler-lib:publish :niddler-lib:bintrayUpload
-./gradlew :niddler-lib-noop:clean :niddler-lib-noop:assembleRelease :niddler-lib-noop:publish :niddler-lib-noop:bintrayUpload
+./gradlew clean :niddler-lib-android:assembleRelease :niddler-lib-android-noop:assembleRelease :niddler-lib-java-noop:assemble :niddler-lib-java:assemble :niddler-lib-base:bintrayUpload :niddler-lib-base-noop:bintrayUpload :niddler-lib-java:bintrayUpload :niddler-lib-java-noop:bintrayUpload :niddler-lib-android:bintrayUpload :niddler-lib-android-noop:bintrayUpload --no-daemon
+
